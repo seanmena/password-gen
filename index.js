724383be635f7b12c060fe55fc357bdc743375be
Sean Mena <seanmena13@gmail.com>
2023-02-17 12:00:32 -0700

    built base

---
+++ index.js
@@ -0,0 +1,88 @@
+let lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+let areNumbers = "1234567890".split("");
+let areSpecial = "!@#$%^&*".split("");
+let upperCheck = document.querySelector("#upper");
+let lowerCheck = document.querySelector("#lower");
+let numCheck = document.querySelector("#number");
+let specCheck = document.querySelector("#special");
+let button = document.querySelector("#button");
+let howmany = document.querySelector("#length");
+let passwordOutput = document.querySelector("#password");
+
+function getUpper() {
+  if (upperCheck.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getLower() {
+  if (lowerCheck.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getNum() {
+  if (numCheck.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getSpec() {
+  if (specCheck.checked) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getLength() {
+  lengthNum = howmany.value;
+  console.log(lengthNum);
+}
+
+function generatePass() {
+  let password = "";
+  let possiblePass = [];
+
+  if (lengthNum < 8) {
+    alert("Must be at least 8 characters");
+  }
+
+  if (getUpper() === true) {
+    possiblePass = possiblePass.concat(upperCase);
+  }
+
+  if (getLower() === true) {
+    possiblePass = possiblePass.concat(lowerCase);
+  }
+
+  if (getNum() === true) {
+    possiblePass = possiblePass.concat(areNumbers);
+  }
+
+  if (getSpec() === true) {
+    possiblePass = possiblePass.concat(areSpecial);
+  }
+
+  while (password.length < Number(lengthNum)) {
+    let randomIndex = Math.floor(Math.random() * possiblePass.length);
+    let randomChar = possiblePass[randomIndex];
+    password += randomChar;
+  }
+
+  passwordOutput.textContent = password;
+  console.log(password);
+  console.log(possiblePass, lengthNum + "working!! hooray!!!");
+}
+
+button.addEventListener("click", () => {
+  getLength();
+  generatePass();
+});
